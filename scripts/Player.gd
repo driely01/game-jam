0d203923e5f20ad08367150d16d728e0eefc2ae9
Abdellah Moukhless <amoukhle@e3r9p6.1337.ma>
2024-08-12 16:17:23 +0100

    add spawn player

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,28 +1,52 @@
 extends CharacterBody2D
 
+var weapon_scene = preload("res://scenes/weapon.tscn")
+var throw_distance = 200.0
+var throw_power = 400.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var sprite_gun : Sprite2D
+var sprite : Sprite2D 
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func _ready():
+	sprite_gun = $player_with_gun
+	sprite = $player_without_gun
 
+func _process(delta):
 
-func _physics_process(delta):
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity.y += gravity * delta
+	look_at(get_global_mouse_position())
+	
+	if Input.is_action_just_pressed("spawn"):
+		throw_weapon()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func throw_weapon():
+	var weapon = weapon_scene.instantiate()
+	weapon.position = $Marker2D.position
+	
+	# Calculate direction from marker to the mouse position
+	var direction = $Marker2D.global_transform.x
+	
+	# Set the weapon's initial velocity
+	if weapon is RigidBody2D:
+		weapon.gravity_scale = 0
+		weapon.initial_velocity = direction * throw_power
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	# Connect the weapon's signal to handle removal
+	weapon.connect("weapon_removed", Callable(self, "_on_weapon_removed"))
 
-	move_and_slide()
+	# Hide the player's sprite and other elements
+	if sprite_gun:
+		sprite_gun.visible = false
+	if sprite:
+		sprite.visible = true
+
+	# Add the weapon to the scene
+	add_child(weapon)
+
+func _on_weapon_removed(gun_position: Vector2):
+	# Respawn the player at the last position of the weapon
+	global_position = gun_position
+	
+	if sprite_gun:
+		sprite_gun.visible = true
+	if sprite:
+		sprite.visible = false
